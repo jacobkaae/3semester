d7ec7a515df888f22c27174ee2a9dfdbf717be01
Jacob Kaae <jacobkaae0511@gmail.com>
2022-09-06 12:45:54 +0200

    x

---
+++ Modul3/Opgave8/Program.cs
@@ -0,0 +1,43 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+Task[] Huskeliste = new Task[]
+{
+    new Task { Id = 1, Text = "Rydde op", Done = false},
+    new Task { Id = 2, Text = "Vaske gulv", Done = false},
+    new Task { Id = 3, Text = "Toiletter", Done = false}
+
+};
+
+// GET /api/tasks
+app.MapGet("/api/task", () => Huskeliste);
+
+// GET /api/tasks/{id}
+app.MapGet("/api/task/{id}", (int id) => Huskeliste.Where(p => p.Id == id));
+
+// PUT /api/tasks/{id}
+app.MapPut("/api/task/{id}", (int id, Task opdTask) =>
+{
+    if (Huskeliste.Where(p => p.Id == id) == null)
+    {
+        Console.WriteLine("Der findes ingen opgave med dette ID");
+
+        return Huskeliste;
+    }
+    else
+    {
+        Huskeliste.Select(x => opdTask).Where(p => p.Id == id);
+
+        return Huskeliste;
+    }
+});
+
+app.Run();
+
+
+public class Task
+{
+    public int Id { get; set; }
+    public string Text { get; set; }
+    public bool Done { get; set; }
+}
