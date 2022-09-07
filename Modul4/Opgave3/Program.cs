14b4519d4b345091d8a7eff6412bf70329e10adc
Jacob Kaae <jacobkaae0511@gmail.com>
2022-09-07 11:52:25 +0200

    x

---
+++ Modul4/Opgave3/Program.cs
@@ -0,0 +1,80 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var AllowCors = "_AllowCors";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: AllowCors, builder => {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+app.UseCors(AllowCors);
+
+int nextId = 0;
+
+Task[] Huskeliste = new Task[]
+{
+    new Task { Id = nextId++, Text = "Rydde", Done = false},
+    new Task { Id = nextId++, Text = "Vaske gulv", Done = false},
+    new Task { Id = nextId++, Text = "Toiletter", Done = false}
+
+};
+
+// GET /api/tasks
+app.MapGet("/api/tasks", () => Huskeliste);
+
+// GET /api/tasks/{id}
+app.MapGet("/api/tasks/{id}", (int id) => Huskeliste.Where(p => p.Id == id));
+
+// PUT /api/tasks/{id}
+app.MapPut("/api/tasks/{id}", (int id, Task opdTask) =>
+{
+    return Huskeliste = Huskeliste.Select(x =>
+    {
+        if (x.Id == id)
+        {
+            x = opdTask;
+            x.Id = id;
+            return x;
+        } else
+        {
+            return x;
+        }
+    }).ToArray();
+
+    // Best practice
+    //return Huskeliste = Huskeliste.Select(x => x.Id == id ? opdTask : x).ToArray();
+
+});
+
+
+// DELETE /api/tasks/{id}
+app.MapDelete("/api/tasks/{id}", (int id) => Huskeliste = Huskeliste.Where(x => x.Id != id).ToArray());
+
+
+// POST /api/tasks/
+app.MapPost("/api/tasks", (Task nyTask) => {
+    nyTask.Id = nextId++;
+    return Huskeliste = Huskeliste.Append(nyTask).ToArray();
+});
+
+app.Run();
+
+
+Spørgsmål[] Quiz = new Spørgsmål[]
+{
+    new Spørgsmål { Id = nextId++, SpgText = "Hvor mange brødre har Jacob?", Svarmuligheder = new string[] { "En", "To", "Tre", "Fire" }, SvarIndex = 1},
+    new Spørgsmål { Id = nextId++, SpgText = "Hvilken drikkevarer har Jacob på foden?", Svarmuligheder = new string[] { "Tequila", "Sambuca", "Bajer", "Vand(lol)"}, SvarIndex = 2}
+};
+
+public class Spørgsmål
+{
+    public long Id { get; set; }
+    public string SpgText { get; set; }
+    public string[] Svarmuligheder { get; set; }
+    public int SvarIndex { get; set; }
+}
+
