474ac773bb334085e15bc6438d0b5b8874e1ae61
Jacob Kaae <jacobkaae0511@gmail.com>
2022-09-20 12:10:53 +0200

    x

---
+++ Modul4/Opgave4/Data/Spørgsmål.cs
@@ -0,0 +1,7 @@
+public class Spørgsmål
+{
+    public long Id { get; set; }
+    public string SpgText { get; set; }
+    public string[] Svarmuligheder { get; set; }
+    public int SvarIndex { get; set; }
+}
