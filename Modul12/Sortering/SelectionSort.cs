3ff1ee68c5517fc56d9262af4a2f03f7423ac5bc
Jacob Kaae <jacobkaae0511@gmail.com>
2022-11-15 11:57:50 +0100

    x

---
+++ Modul12/Sortering/SelectionSort.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Sortering;
+
+public class SelectionSort
+{
+    private static void Swap(int[] array, int i, int j)
+    {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void Sort(int[] array)
+    {
+        // TODO!
+        for (int i = 0; i < array.Length; i++)
+        {
+            int min = i;
+
+            // Find the mindst tal
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[j] < array[min])
+                {
+                    min = j;
+                }
+            }
+
+            Swap(array, i, min);
+
+        }
+
+        return;
+
+    }
+
+}
+    
+
