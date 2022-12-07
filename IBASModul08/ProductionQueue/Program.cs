4120c681f91678d006c64dca03f17924a17adf07
Jacob Kaae <jacobkaae0511@gmail.com>
2022-12-07 11:11:28 +0100

    Vikrer

---
+++ IBASModul08/ProductionQueue/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.Amqp;
+using Microsoft.Azure.Amqp.Framing;
+
+namespace QueueSender
+{
+    public class Bajer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Smag { get; set; }
+    }
+
+    class Program
+    {
+        // connection string to your Service Bus namespace 
+        static string connectionString = "Endpoint=sb://ibasnamespace55.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=gqsoaF93vX4QRVJqfLpvCSGXTYhBezQklsY9Q6J3dnw=";
+
+        // name of your Service Bus queue 
+        static string queueName = "ibasproductionqueue";
+
+        // the client that owns the connection and can be used to create senders and receivers
+        static ServiceBusClient? client;
+
+        // the sender used to publish messages to the queue 
+        static ServiceBusSender? sender;
+
+        // number of messages to be sent to the queue 
+        private const int numOfMessages = 3;
+
+        static async Task Main()
+        {
+            // The Service Bus client types are safe to cache and use as a singleton for the lifetime
+            // of the application, which is best practice when messages are being published or read
+            // regularly. 
+
+            // Create the clients that we'll use for sending and processing messages.
+            client = new ServiceBusClient(connectionString);
+            sender = client.CreateSender(queueName);
+
+
+            // create a batch  
+            using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+            // Serialize C# object
+            List<Bajer> bajere = new List<Bajer> {
+            new Bajer { ID = 1, Name = "Tuborg Classic", Smag = "God" },
+            new Bajer { ID = 2, Name = "Heiken", Smag = "Brækker mig" },
+            new Bajer { ID = 3, Name = "1669 Blanc", Smag = "Ku godt" },
+            new Bajer { ID = 4, Name = "Guinnees", Smag = "Ved ikke helt endnu" }
+            };
+
+
+            foreach (var bajer in bajere)
+            {
+                string jsonString = JsonSerializer.Serialize(bajer);
+
+                // try adding a message to the batch 
+                if (!messageBatch.TryAddMessage(new ServiceBusMessage(jsonString)))
+                {
+                    // if it is too large for the batch 
+                    throw new Exception($"The message is too large to fit in the batch.");
+                }
+            }            
+               
+            try
+            {
+                // Use the producer client to send the batch of messages to the Service Bus queue
+                await sender.SendMessagesAsync(messageBatch);
+                Console.WriteLine($"A batch of {bajere.Count()} messages has been published to the queue.");
+            }
+            finally
+            {
+                // Calling DisposeAsync on client types is required to ensure that network
+                // resources and other unmanaged objects are properly cleaned up. 
+                await sender.DisposeAsync();
+                await client.DisposeAsync();
+            }
+
+            Console.WriteLine("Press any key to end the application");
+            Console.Read();
+        }
+    }
+}
---
+++ IBASModul08/ProductionQueueReceive/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.Amqp;
+using Microsoft.Azure.Amqp.Framing;
+
+namespace QueueReceiver
+{
+    public class Bajer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Smag { get; set; }
+    }
+
+    class Program
+    {
+        // connection string to your Service Bus namespace 
+        static string connectionString = "Endpoint=sb://ibasnamespace55.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=gqsoaF93vX4QRVJqfLpvCSGXTYhBezQklsY9Q6J3dnw=";
+
+        // name of your Service Bus queue 
+        static string queueName = "ibasproductionqueue";
+
+
+        // the client that owns the connection and can be used to create senders and receivers
+        static ServiceBusClient? client;
+
+        // the processor that reads and processes messages from the queue 
+        static ServiceBusProcessor? processor;
+
+        // handle received messages 
+        static async Task MessageHandler(ProcessMessageEventArgs args)
+        {
+            string body = args.Message.Body.ToString();
+
+
+            Bajer ølDe = JsonSerializer.Deserialize<Bajer>(body);
+
+            Console.WriteLine($"Received: ID: {ølDe.ID}, Name: {ølDe.Name}, Smag: {ølDe.Smag}");
+
+            // complete the message. messages is deleted from the queue.  
+            await args.CompleteMessageAsync(args.Message);
+        }
+
+        // handle any errors when receiving messages 
+        static Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+
+        static async Task Main()
+        {
+            // The Service Bus client types are safe to cache and use as a singleton for the lifetime
+            // of the application, which is best practice when messages are being published or read
+            // regularly. 
+
+            // Create the client object that will be used to create sender and receiver objects
+
+            client = new ServiceBusClient(connectionString);
+
+            // create a processor that we can use to process the messages 
+            processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+
+            try
+            {
+                // add handler to process messages 
+                processor.ProcessMessageAsync += MessageHandler;
+
+                // add handler to process any errors 
+                processor.ProcessErrorAsync += ErrorHandler;
+
+                // start processing  
+                await processor.StartProcessingAsync();
+
+                Console.WriteLine("Wait for a minute and then press any key to end the processing");
+                Console.Read();
+
+                // stop processing  
+                Console.WriteLine("\nStopping the receiver...");
+                await processor.StopProcessingAsync();
+                Console.WriteLine("Stopped receiving messages");
+            }
+            finally
+            {
+                // Calling DisposeAsync on client types is required to ensure that |network 
+                // resources and other unmanaged objects are properly cleaned up. 
+                await processor.DisposeAsync();
+                await client.DisposeAsync();
+            }
+        }
+    }
+}
